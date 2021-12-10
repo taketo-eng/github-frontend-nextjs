d5b006a086fb82a20a9936f7b6659fe76a39f3ee
Taketo Yahiro <yahiro.t.eng@gmail.com>
2021-12-10 15:36:14 +0900

    initial commit

---
+++ __tests__/Home.test.js
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment json
+ */
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages/index'
+
+it('Should render title text', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!')).toBeInTheDocument()
+})
